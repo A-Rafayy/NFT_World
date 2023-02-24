dd21dd2d4038410ad29f35702b358d9ad4ea4a68
Rafayyyyy <a.rafay.suhail@gmail.com>
2023-02-24 17:20:19 +0500

    NFT MarketPlace

---
+++ components/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import {CircleButton, RectangleButton} from './Button';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+import {SubInfo} from './SubInfo';
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectangleButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
